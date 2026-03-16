2f68f2c5644cf0d3c6cb61be96e69164440124a5
Mohammed Nihal <mnkv1013@gmail.com>
2026-03-16 10:17:46 +0530

    changes

---
+++ run_pup.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('pageerror', err => console.log('PAGE ERROR:', err.message));
+  await page.goto('file:///n:/%230Brinit/Arc-web/index.html');
+  await browser.close();
+})();
